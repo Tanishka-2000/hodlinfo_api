0b6ac6d27c838dc4fe399fd46ef5eb160b58617f
Tanishka <tanishkatalwar18@gmail.com>
2023-09-19 16:15:07 +0530

    fix decimal128 type to number for decimal values

--- models/crypto.js
+++ models/crypto.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const cryptoSchema = new Schema({
     base_unit: String,
-    last: Schema.Types.Decimal128,
-    volume: Schema.Types.Decimal128,
-    sell:Schema.Types.Decimal128,
-    buy: Schema.Types.Decimal128,
+    last: Number,
+    volume: Number,
+    sell:Number,
+    buy: Number,
     name: String
 });
 
